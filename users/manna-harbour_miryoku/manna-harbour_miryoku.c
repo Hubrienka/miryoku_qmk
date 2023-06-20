a820e690904e8d40f2e47fd94fcb6affed9f5c02
Hubrienka <54112033+Hubrienka@users.noreply.github.com>
2023-06-20 22:39:02 +0200

    Update manna-harbour_miryoku.c

--- users/manna-harbour_miryoku/manna-harbour_miryoku.c
+++ users/manna-harbour_miryoku/manna-harbour_miryoku.c
@@ -7,6 +7,14 @@
 
 #include "manna-harbour_miryoku.h"
 
+#include "features/achordion.h"
+
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+  if (!process_achordion(keycode, record)) { return false; }
+  // Your macros ...
+
+  return true;
+}
 
 // Additional Features double tap guard
 
