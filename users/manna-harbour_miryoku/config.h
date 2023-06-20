a77931061eb0fe703d6984384933dea5ccdcdcf6
Hubrienka <54112033+Hubrienka@users.noreply.github.com>
2023-06-20 23:31:08 +0200

    Update config.h

--- users/manna-harbour_miryoku/config.h
+++ users/manna-harbour_miryoku/config.h
@@ -5,17 +5,15 @@
 
 #pragma once
 
-#include "custom_config.h"
-
 // default but used in macros
 #undef TAPPING_TERM
-#define TAPPING_TERM 180
+#define TAPPING_TERM 160
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-#define QUICK_TAP_TERM 180
+#define QUICK_TAP_TERM 160
 
 // Auto Shift
 #define NO_AUTO_SHIFT_ALPHA
