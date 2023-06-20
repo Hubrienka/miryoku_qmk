b136bd2eadc5b463abe819967710e2a20926f0eb
Hubrienka <54112033+Hubrienka@users.noreply.github.com>
2023-06-20 22:40:00 +0200

    Update manna-harbour_miryoku.c

--- users/manna-harbour_miryoku/manna-harbour_miryoku.c
+++ users/manna-harbour_miryoku/manna-harbour_miryoku.c
@@ -16,6 +16,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
   return true;
 }
 
+void matrix_scan_user(void) {
+  achordion_task();
+}
+
 // Additional Features double tap guard
 
 enum {
